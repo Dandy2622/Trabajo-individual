86dd9caab8c65f4951fe80b746e82c721602cb9f
Dandy2622 <danielvasquezgonzales@outlook.com>
2021-09-11 15:14:41 -0300

    colores
    se agrega boton para colores

--- css/script.js
+++ css/script.js
@@ -1,3 +1,16 @@
 Swal.fire ({
     title :  "BIENVENIDO A NUESTRO MUNDO VERDE"
 })
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+	document.getElementById("hexadecimal").innerHTML = color;
+	document.getElementById("text").innerHTML = "Copiar Color";
+}
